43f7e08aabde231d7e951feaaf2f280f8332bc54
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-09 11:35:56 +1000

    Before removing template files

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CSharpDockerHelloWorld.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
