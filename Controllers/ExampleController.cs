fb9098c876fa83ea596b766ca8165715fe3ee9fc
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-01 15:34:45 +1000

    Fixed port listening

--- Controllers/ExampleController.cs
+++ Controllers/ExampleController.cs
@@ -9,7 +9,7 @@ namespace CSharpDockerHelloWorld.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Hello World!";
+            return "Hello world!";
         }
     }
 }
