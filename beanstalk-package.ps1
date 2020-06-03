2b4f3d543f6e0484ee7d165634bd6a3a59481c72
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-04 08:21:21 +1000

    Fixed script

--- beanstalk-package.ps1
+++ beanstalk-package.ps1
@@ -1,3 +1,3 @@
 dotnet publish -c Release
-Compress-Archive -Path ./bin\Release\netcoreapp3.1\publish -DestinationPath csharphelloworld.zip -Force
+Compress-Archive -Path ./bin\Release\netcoreapp3.1\publish\* -DestinationPath csharphelloworld.zip -Force
 Compress-Archive -Path csharphelloworld.zip, aws-windows-deployment-manifest.json -DestinationPath beanstalk-app-bundle.zip -Force
